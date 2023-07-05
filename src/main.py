aa67d989e8d9c453ecdc1383f8842ac2177fd332
lzhalloran <lzhalloran@gmail.com>
2023-07-06 02:36:56 +1000

    added some intial code to main.py to form the title/menu screen

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("Running main.py...")
+import os
+terminal_width = 80
+terminal_height = 24
+title = "Warehouse Inventory Manager"
+
+
+os.system('cls||clear')
+print("_" * terminal_width)
+print()
+print(" " * int((terminal_width/2 - len(title)/2)) + title)
+print("\n" * (terminal_height - 6))
+print("_" * terminal_width)
+
+input()
+os.system('cls||clear')
