30550e88a3b85a4410b4fe70d831dd327a9d0947
lzhalloran <lzhalloran@gmail.com>
2023-07-06 01:41:30 +1000

    added checks for python3 and pip install, and virtual environment to shell

--- src/shell.sh
+++ src/shell.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 if ! [[ -x "$(command -v python3)" ]]
 then
-  echo 'This program runs on Python3, but it looks like Python3 is not installed.
+    echo 'This program runs on Python3, but it looks like Python3 is not installed.
     Installing Python3...'
-  exit 1
+    sudo apt-get update
+    sudo apt-get install python3
 fi
 
+if ! [[ -x "$(command -v pip)" ]]
+then
+    echo 'This program requires pip installer, but it looks like pip is not installed.
+    Installing pip...'
+    sudo apt-get install python3-pip
+fi
+
+python3 -m venv .venv
+source .venv/bin/activate
+
 python3 main.py
+
+deactivate
+
+rm -rf .venv .pytest_cache
