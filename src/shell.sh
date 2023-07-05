0f9916c95bf90c59c7a0b452f19d718be9642912
lzhalloran <lzhalloran@gmail.com>
2023-07-06 01:16:05 +1000

    added initial code to main.py and shell.sh to link them together

--- src/shell.sh
+++ src/shell.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+if ! [[ -x "$(command -v python3)" ]]
+then
+  echo 'This program runs on Python3, but it looks like Python3 is not installed.
+    Installing Python3...'
+  exit 1
+fi
+
+python3 main.py
