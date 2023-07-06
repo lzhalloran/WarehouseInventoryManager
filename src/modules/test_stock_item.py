4e984e64724abca531e6109f96f577e70b17c6c9
lzhalloran <lzhalloran@gmail.com>
2023-07-07 02:43:18 +1000

    started unit test for stock_item class

---
+++ src/modules/test_stock_item.py
@@ -0,0 +1,15 @@
+import pytest
+from stock_item import StockItem
+
+class TestStockItem:
+    def test_stock_item_initial_value():
+        identifier = 123456789
+        product = 12054
+        location = ["Room1", 4, 1]
+
+        item = StockItem(identifier, product, location)
+        assert [item.identifier, item.product, item.location] == [identifier, product, location]
+    
+    def test_stock_item_no_value():
+        with pytest.raises(Exception) as error_info:
+            item = StockItem()
