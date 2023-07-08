959a6edfb099bf224c175bf6196ea446590c711a
lzhalloran <lzhalloran@gmail.com>
2023-07-08 17:37:35 +1000

    altered stock item attributes to properties, made identifier and product read-only

--- src/modules/stock_item.py
+++ src/modules/stock_item.py
@@ -3,14 +3,30 @@ import value_checker
 class StockItem:
     def __init__(self, identifier: int, product: int, location):
         if value_checker.is_non_negative_integer(identifier):
-            self.identifier = identifier
+            self._identifier = identifier
         else:
             raise ValueError("Identifier must be a non-negative integer value")
         if value_checker.is_non_negative_integer(product):
-            self.product = product
+            self._product = product
         else:
             raise ValueError("Product must be a non-negative integer value")
-        if value_checker.is_non_empty_string(location[0]) and value_checker.is_non_negative_integer(location[1]) and value_checker.is_non_negative_integer(location[2]):
-            self.location = location
+        self.location = location
+        
+    @property
+    def identifier(self):
+        return self._identifier
+    
+    @property
+    def product(self):
+        return self._product
+    
+    @property
+    def location(self):
+        return self._location
+    
+    @location.setter
+    def location(self, value):
+        if value_checker.is_non_empty_string(value[0]) and value_checker.is_non_negative_integer(value[1]) and value_checker.is_non_negative_integer(value[2]):
+            self._location = value
         else:
             raise ValueError("Location must have following format: [Roomname (non-empty string), Row (non-negative integer), Height (non-negative integer)]")
