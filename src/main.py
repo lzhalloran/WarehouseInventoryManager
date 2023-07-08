0de55ab51f61128f3693e828c9e0463075eb9328
lzhalloran <lzhalloran@gmail.com>
2023-07-08 18:40:53 +1000

    added basic main menu to main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,36 @@
 import os
 import sys
 sys.path.append("modules")
-terminal_width = 80
-terminal_height = 24
+
+TERMINAL_WIDTH = 80
+TERMINAL_HEIGHT = 24
 title = "Warehouse Inventory Manager"
 
+def clear_screen():
+    os.system('cls||clear')
 
-os.system('cls||clear')
-print("-" * terminal_width)
-print(" " * int((terminal_width/2 - len(title)/2)) + title)
-print("-" * terminal_width)
-print("\n" * (terminal_height - 6))
-print("_" * terminal_width)
-input()
-os.system('cls||clear')
+def display_title():
+    print("-" * TERMINAL_WIDTH)
+    print(" " * int((TERMINAL_WIDTH/2 - len(title)/2)) + title)
+    print("-" * TERMINAL_WIDTH)
+
+def display_option(number: int, text: str):
+    print(" " * int((TERMINAL_WIDTH/2 - 10)) + "[" + str(number) + "] - " + text + "\n")
+
+
+def display_main_menu():
+    display_title()
+    print("\n" * 6)
+    display_option(1, "Products")
+    display_option(2, "Stock")
+    display_option(3, "Exit")
+    print("\n" * (TERMINAL_HEIGHT - 19))
+    print("_" * TERMINAL_WIDTH)
+
+user_input = ""
+while(user_input != "3"):
+    clear_screen()
+    display_main_menu()
+    user_input = input("Choose option: ")
+
+clear_screen()
